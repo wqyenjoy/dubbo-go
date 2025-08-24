cbfb1a820ea50e926c563798785925212786c20a
john <>
2025-08-24 19:00:30 +0800

    style: format imports with three separate blocks
    - Standard library packages (context, strconv, strings)
    - Third-party packages (github.com/dubbogo/gost/log/logger, github.com/pkg/errors)
    - Project internal packages (dubbo.apache.org/dubbo-go/v3/...)
    
    This follows the project import formatting standards for better code organization.

--- metadata/metadata_service.go
+++ metadata/metadata_service.go
@@ -21,10 +21,14 @@ import (
 	"context"
 	"strconv"
 	"strings"
+)
 
+import (
 	"github.com/dubbogo/gost/log/logger"
 	perrors "github.com/pkg/errors"
+)
 
+import (
 	"dubbo.apache.org/dubbo-go/v3/common"
 	"dubbo.apache.org/dubbo-go/v3/common/constant"
 	"dubbo.apache.org/dubbo-go/v3/common/extension"
